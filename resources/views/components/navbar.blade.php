93314df72dce128207b1d29862e885183e8face2
Anro128 <ahmadnurrohim2812@gmail.com>
2023-11-18 11:30:54 +0700

    add component 'navbar' (biar kalo ngedit navbar cukup sekali) *revisi dikit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,3 @@
-<div>
 <div class="container position-sticky z-index-sticky top-0">
       <div class="row">
         <div class="col-12">
@@ -122,5 +121,4 @@
           </nav>
         </div>
       </div>
-    </div>
 </div>
