15e22abde4f204e84f856c9d1c678b5d0b24d91d
RyanM-01 <ryan.m.syahran@gmail.com>
2023-11-21 19:57:08 +0700

    Update!

--- resources/views/kandang/input.blade.php
+++ resources/views/kandang/input.blade.php
@@ -1,140 +1,104 @@
 <!--
-=========================================================
-* Material Kit 2 - v3.0.4
-=========================================================
-
-* Product Page:  https://www.creative-tim.com/product/material-kit 
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
-<!DOCTYPE html>
+   =========================================================
+   * Material Kit 2 - v3.0.4
+   =========================================================
+   
+   * Product Page:  https://www.creative-tim.com/product/material-kit 
+   * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+   * Coded by www.creative-tim.com
+   
+    =========================================================
+   
+   * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+   <!DOCTYPE html>
 <html lang="en" itemscope itemtype="http://schema.org/WebPage">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="{{Vite::asset('resources/images/logo.png')}}">
-    <title>Input Kandang</title>
-    <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <!-- CSS Files -->
-    @vite('resources/css/material-kit.css')
-    <!-- Nepcha Analytics (nepcha.com) -->
-    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
-    <style>
-       body {
-      background-color: #f4f4f4;
-      background-size: cover;
-      background-repeat: no-repeat;
-      width: 100%;
-      height: 150vh;
-    }
-      .box {
-      background-color: rgba(255, 255, 255, 0.8); 
-      border-radius: 8px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      padding: 20px;
-      margin: 50px auto;
-      transform:translateY(10%);
-      width:700px;
-      height:700px;
-  }
-
-     form {
-    max-width: 400px; /* Adjust the width as needed */
-    margin: 0 auto;
-    }
-
-/* Add additional styling for form elements if needed */
-label {
-    display: block;
-    margin-bottom: 8px;
-}
-
-input,
-select,
-textarea {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 16px;
-    box-sizing: border-box;
-
-}
-
-
-#deskripsi {
-    width: 100%;
-    height: 200px; /* Adjust the height as needed */
-    padding: 8px;
-    box-sizing: border-box;
-}
-
-
-.card-header {
-    width: 100%; /* Adjust the max-width as needed */
-    margin: 0 auto;
-    line-height: 10px; /* Adjust the line height as needed */
-}
-
-.larger-box {
-    background: linear-gradient(87deg,#74f52f 0, #118F6A 100%);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    border-radius: 15px;
-    padding: 20px; /* Adjust padding for content inside the larger box */
-    text-align: center; /* Center the content inside the larger box */
-}
-
-.larger-box h2 {
-    margin: 5px;
-    top: 0;
-    color:#ffff;
-    font-family: 'Courier New', monospace;"
-}
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="icon" href="{{Vite::asset('resources/images/logo.png')}}">
+      <title>Input Kandang | GembalaCerdas</title>
+      <!--     Fonts and icons     -->
+      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+      <!-- Font Awesome Icons -->
+      <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+      <!-- Material Icons -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+      <!-- CSS Files -->
+      @vite('resources/css/material-kit.css')
+      <!-- Nepcha Analytics (nepcha.com) -->
+      <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+      <style>
         </style>
-<body style="background-image: url('{{Vite::asset('resources/images/background3.png')}}');">
-<span class="mask bg-gradient-dark opacity-6" style= height:150vh;></span>
-    <header>
-    <x-navbar/>
-</header>
-<main>
-  <div class="box">
-  <div class="card-header">
-              <div class="larger-box">
-              <h2> Input Kandang </h2>
-              </div>
+      <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+   </head>
+   <body class="overlay">
+      <x-navbar/>
+      <div class="page-header align-items-start min-vh-100" style="background-image: url({{Vite::asset('resources/images/background2.png')}});" loading="lazy">
+         <div class="row mx-auto">
+            <div class="col-lg-7 text-center mx-auto" style="margin-top: 150px">
+               <h3 class="text-white pt-3 mt-n5">Input Kandang</h3>
             </div>
-            <label for="nama_kandang">Nama Kandang:</label>
-            <input type="text" id="nama_kandang" name="nama_kandang" required>
+            
+            <div class="mx-auto" style="margin: 10px; ">
+            <!--- <form action="{{ url('/riwayat/kawin/add') }}" method="POST">
+                  @csrf ntar ubah yh -->
+                  <section class="py-1">
+                     <div class="row justify-space-between py-2">
+                        <div class=" mx-auto">
+                           <div class="input-group input-group-static mb-1">
+                              <label for="id_ternak" style="color:white" >Ternak</label> <img src="{{Vite::asset('resources/images/down-arrow-white.svg')}}" alt="down-arrow" class="arrow ms-auto ms-md-2">
+                              <select class="form-control" id="id_ternak" name="id_ternak"  style="color:white" required>
+                              <option value="" >Select Ternak</option>
+                              <!-- Add options as needed -->
+                              </select>
+                           </div>
+                        </div>
+                     </div>
+                  </section>
 
-            <label for="ternak">Ternak:</label>
-            <select id="ternak" name="ternak">
-            <option value="test2">ternak1</option>
-            <option value="test1">ternak2</option>
-            </select>
-            <label for="kapasitas">Kapasitas:</label>
-            <input type="number" id="kapasitas" name="kapasitas" required>
-            <label for="deskripsi">Deskripsi:</label>
-            <textarea id="deskripsi" name="deskripsi" required></textarea>
+                  <section class="py-1">
+    <div class="row justify-space-between py-2">
+        <div class=" mx-auto">
+            <div class="input-group input-group-static mb-1">
+                <label for="nama_kandang" style="color:white" >Nama Kandang</label> 
+                <input type="text" class="form-control" id="nama_kandang" name="nama_kandang"  style="color:white" required placeholder="Enter Nama Kandang">
+            </div>
+        </div>
+    </div>
+</section>
 
-            <button type="submit" class="btn bg-gradient-success mb-0 me-1 mt-2 mt-md-0" style="background: linear-gradient(87deg, #74f52f 0, #118F6A 100%);">Submit</button>
+<section class="py-1">
+    <div class="row justify-space-between py-2">
+        <div class=" mx-auto">
+            <div class="input-group input-group-static mb-1">
+                <label for="kapasitas" style="color:white" >Kapasitas</label> 
+                <input type="number" class="form-control" id="kapasitas" name="kapasitas"  style="color:white" required placeholder="Enter Kapasitas">
+            </div>
+        </div>
+    </div>
+</section>
 
-  </div>
-</main>
+<section class="py-1">
+    <div class="row justify-space-between py-2">
+        <div class=" mx-auto">
+            <div class="input-group input-group-static mb-1">
+                <label for="deskripsi" style="color:white" >Deskripsi Kandang</label> 
+                <textarea class="form-control" id="deskripsi" name="deskripsi" style="color:white" required placeholder="Enter Deskripsi"></textarea>
+            </div>
+        </div>
+    </div>
+</section>
 
-
-
-
-
-
-
-
-
+<div class="row py-1">
+    <div class="mx-auto">
+        <div class="input-group input-group-static">
+            <button type="submit" class="btn bg-gradient-success mb-0 me-1 mt-2 mt-md-0">Submit</button>
+        </div>
+    </div>
+</div>
+</form>
+</div>
+</div>
+</div>
 </body>
-</hmtl>
+</html>
--- resources/views/kandang/pindahkandang.blade.php
+++ resources/views/kandang/pindahkandang.blade.php
@@ -1,138 +1,88 @@
 <!--
-=========================================================
-* Material Kit 2 - v3.0.4
-=========================================================
-
-* Product Page:  https://www.creative-tim.com/product/material-kit 
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
-<!DOCTYPE html>
+   =========================================================
+   * Material Kit 2 - v3.0.4
+   =========================================================
+   
+   * Product Page:  https://www.creative-tim.com/product/material-kit 
+   * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+   * Coded by www.creative-tim.com
+   
+    =========================================================
+   
+   * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+   <!DOCTYPE html>
 <html lang="en" itemscope itemtype="http://schema.org/WebPage">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="{{Vite::asset('resources/images/logo.png')}}">
-    <title>Pindah Kandang</title>
-    <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <!-- CSS Files -->
-    @vite('resources/css/material-kit.css')
-    <!-- Nepcha Analytics (nepcha.com) -->
-    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
-    <style>
-              body {
-            background-color: #f4f4f4;
-            background-size: cover;
-            background-repeat: no-repeat;
-            width: 100%;
-            height: 120vh;
-          }
-            .transparent-form {
-            background-color: rgba(255, 255, 255, 0.8); /* Adjust the transparency */
-            padding: 20px;
-            border-radius: 10px;
-            width: 750px; /* Adjust the width as needed */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-            margin: 50px auto;
-            transform:translateY(20%);
-        }
-
-        .form-group {
-            margin-bottom: 15px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        select, textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-sizing: border-box;
-        }
-
-        button {
-            background: linear-gradient(87deg, #74f52f 0, #118F6A 100%);
-            color: #fff;
-            padding: 10px 15px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #45a049;
-        }
-        .card-header {
-    width: 100%; /* Adjust the max-width as needed */
-    margin: 0 auto;
-    line-height: 10px; /* Adjust the line height as needed */
-        }
-
-        .larger-box {
-            background: linear-gradient(87deg,#74f52f 0, #118F6A 100%);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 15px;
-            padding: 20px; /* Adjust padding for content inside the larger box */
-            text-align: center; /* Center the content inside the larger box */
-        }
-
-        .larger-box h2 {
-            margin: 5px;
-            top: 0;
-            color:#ffff;
-            font-family: 'Courier New', monospace;"
-        }
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="icon" href="{{Vite::asset('resources/images/logo.png')}}">
+      <title>Pindah Kandang | GembalaCerdas</title>
+      <!--     Fonts and icons     -->
+      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+      <!-- Font Awesome Icons -->
+      <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+      <!-- Material Icons -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+      <!-- CSS Files -->
+      @vite('resources/css/material-kit.css')
+      <!-- Nepcha Analytics (nepcha.com) -->
+      <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+      <style>
         </style>
-<body style="background-image: url('{{Vite::asset('resources/images/background3.png')}}');">
-<span class="mask bg-gradient-dark opacity-6" style= height:120vh;></span>
-                <header>
-                <x-navbar/>
-            </header>
-            <main>
-            <div class="transparent-form">
-            <div class="card-header">
-              <div class="larger-box">
-              <h2> Pindah Kandang </h2>
-              </div>
+      <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+   </head>
+   <body class="overlay">
+      <x-navbar/>
+      <div class="page-header align-items-start min-vh-100" style="background-image: url({{Vite::asset('resources/images/background2.png')}});" loading="lazy">
+         <div class="row mx-auto">
+            <div class="col-lg-7 text-center mx-auto" style="margin-top: 150px">
+               <h3 class="text-white pt-3 mt-n5">Pindah Kandang</h3>
             </div>
-        <div class="form-group">
-            <label for="selectTernak">Pilih Ternak:</label>
-            <select id="selectTernak" name="selectTernak">
-                <option value="ternak1">Ternak 1</option>
-                <option value="ternak2">Ternak 2</option>
-                <!-- Add more options as needed -->
-            </select>
-        </div>
+            
+            <div class="mx-auto" style="margin: 10px; ">
+            <!--- <form action="{{ url('/riwayat/kawin/add') }}" method="POST">
+                  @csrf ntar ubah yh -->
+                  <section class="py-1">
+                     <div class="row justify-space-between py-2">
+                        <div class=" mx-auto">
+                           <div class="input-group input-group-static mb-1">
+                              <label for="id_ternak" style="color:white" >Ternak</label> <img src="{{Vite::asset('resources/images/down-arrow-white.svg')}}" alt="down-arrow" class="arrow ms-auto ms-md-2">
+                              <select class="form-control" id="id_ternak" name="id_ternak"  style="color:white" required>
+                              <option value="" >Select Ternak</option>
+                              <!-- Add options as needed -->
+                              </select>
+                           </div>
+                        </div>
+                     </div>
+                  </section>
 
-        <div class="form-group">
-        <label for="kandang-asal">Kandang asal:</label>
-        <input type="text" id="kandang-asal" name="kandang-asal" placeholder="Enter kandang asal" value="autofill saat memilih ternak" style="width:250px; border-radius:8px;">
-         </div>
+                  <section class="py-1">
+        <div class="row justify-space-between py-2">
+            <div class=" mx-auto">
+                <div class="input-group input-group-static mb-1">
+                    <label for="nama_kandang" style="color:white" >Asal Kandang</label> 
+                        <input type="text" class="form-control" id="nama_kandang" name="nama_kandang"  style="color:white" value ="Autofill saat milih ternak" required >
+                    </div>
+                </div>
+            </div>
+        </section>
 
-
-        <div class="form-group">
-            <label for="selectKandangTujuan">Kandang Tujuan:</label>
-            <select id="selectKandangTujuan" name="selectKandangTujuan">
-                <option value="kandang1">Kandang 1</option>
-                <option value="kandang2">Kandang 2</option>
-                <!-- Add more options as needed -->
-            </select>
-        </div>
-
-        <button type="submit">Submit</button>
-    </div>
-
-</main>
+        <section class="py-1">
+                     <div class="row justify-space-between py-2">
+                        <div class=" mx-auto">
+                           <div class="input-group input-group-static mb-1">
+                              <label for="kandang_tujuan" style="color:white" >Kandang Tujuan</label> <img src="{{Vite::asset('resources/images/down-arrow-white.svg')}}" alt="down-arrow" class="arrow ms-auto ms-md-2">
+                              <select class="form-control" id="kandang_tujuan" name="kandang_tujuan"  style="color:white" required>
+                              <option value="" >Pilih Kandang Tujuan</option>
+                              <!-- Add options as needed -->
+                              </select>
+                           </div>
+                        </div>
+                     </div>
+                  </section>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
